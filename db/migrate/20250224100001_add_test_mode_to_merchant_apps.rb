4bb4746d11307fb48db18de15e01883e805762f8
Orion <131159175+moyan78641@users.noreply.github.com>
2026-02-22 20:01:11 +0800

    Delete db/migrate directory

--- db/migrate/20250224100001_add_test_mode_to_merchant_apps.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddTestModeToMerchantApps < ActiveRecord::Migration[7.0]
-  def change
-    add_column :credit_merchant_apps, :test_mode, :boolean, default: false
-  end
-end
